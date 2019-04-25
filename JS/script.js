290e0ccddeed060ff5441f2858bddd2f992fcc52
Cris Acosta <iamcris87@gmail.com>
2019-04-25 14:28:59 +0800

    end of first part of DOM crash course

--- JS/script.js
+++ JS/script.js
@@ -42,7 +42,7 @@
 //   items[i].style.backgroundColor = "yellow";
 // }
 
-// items.forEach(item => (item.style.backgroundColor = "blue")); this gives error
+// items.forEach(item => (item.style.backgroundColor = "blue"));// this gives error
 
 // getElementsByTagName //
 
@@ -210,11 +210,67 @@ NOTE: ES6 array function only works on nodelists
 // console.log(e.target.classList);
 // const output = document.getElementById("output");
 // output.innerHTML = "<h3>" + e.target.id + "</h3>";
-// console.log(e.clientX); from the window
+// console.log(e.type) //event type
+
+// mouse positions from the window
+// console.log(e.clientX);
 // console.log(e.clientY);
+
+// mouse positions on the actual element
 // console.log(e.offsetX);
 // console.log(e.offsetY);
 // console.log(e.altKey);
 // console.log(e.ctrlKey);
 // console.log(e.shiftKey);
 // }
+
+/**** MOUSE EVENTS *****/
+
+const button = document.querySelector("#button");
+const box = document.getElementById("box");
+
+// button.addEventListener("click", runEvent);
+// button.addEventListener("dblclick", runEvent);
+// button.addEventListener("mousedown", runEvent);
+// button.addEventListener("mouseup", runEvent);
+
+// box.addEventListener("mouseenter", runEvent);
+// box.addEventListener("mouseleave", runEvent);
+
+// box.addEventListener("mouseover", runEvent);
+// box.addEventListener("mouseout", runEvent)
+
+// box.addEventListener("mousemove", runEvent);
+
+var itemInput = document.querySelector('input[type="text"]');
+var form = document.querySelector("form");
+var select = document.querySelector("select");
+
+// itemInput.addEventListener("keydown", runEvent);
+// itemInput.addEventListener("keyup", runEvent);
+// itemInput.addEventListener("keypress", runEvent);
+// itemInput.addEventListener("focus", runEvent); //focus is when you click on an input
+// itemInput.addEventListener("blur", runEvent);
+// itemInput.addEventListener("cut", runEvent);
+// itemInput.addEventListener("paste", runEvent);
+// itemInput.addEventListener("input", runEvent);
+// select.addEventListener("change", runEvent);
+// select.addEventListener("input", runEvent);
+
+form.addEventListener("submit", runEvent);
+
+function runEvent(e) {
+  e.preventDefault();
+  console.log("EVENT TYPE: " + e.type);
+
+  // console.log(e.target.value);
+  // document.getElementById("output").innerHTML =
+  //   "<h3>" + e.target.value + "</h3>";
+
+  // output.innerHTML =
+  //   "<h3>MouseX: " + e.offsetX + "</h3><h3>MouseY: " + e.offsetY + "</h3>";
+
+  // box.style.backgroundColor = "rgb(" + e.offsetX + "," + e.offsetY + ", 40)";
+  // document.body.style.backgroundColor =
+  // "rgb(" + e.offsetX + "," + e.offsetY + ", 40)";
+}
